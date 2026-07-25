31b0b87a5b7c6e9030fa70ac86242fdfd0bd4100
Heugene <yttghgotthar@gmail.com>
2026-07-25 18:59:59 +0300

    Added more components and routing

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -5,17 +5,19 @@ import { Nav } from "../layout/nav/nav";
 import { AccountService } from '../core/services/account-service';
 import { Home } from '../features/home/home';
 import { User } from '../types/user';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [Nav, Home]
+  imports: [Nav, RouterOutlet]
 })
 export class App implements OnInit {
   private accountService = inject(AccountService);
   private http = inject(HttpClient);
 
+  protected router = inject(Router);
   protected readonly title = signal('Dating App');
   protected members = signal<User[]>([])
 
