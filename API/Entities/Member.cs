4986d8913f6e4303f4f9bdd4cda3ddf945d0612a
Heugene <yttghgotthar@gmail.com>
2026-07-30 20:39:58 +0300

    Excluded some properties from API responses

--- API/Entities/Member.cs
+++ API/Entities/Member.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace API.Entities;
@@ -17,8 +18,10 @@ public class Member
     public required string Country { get; set; }
 
     // Navigation property
+    [JsonIgnore]
     public List<Photo> Photos { get; set; } = [];
 
+    [JsonIgnore]
     [ForeignKey(nameof(Id))]
     public AppUser User { get; set; } = null!;
 }
