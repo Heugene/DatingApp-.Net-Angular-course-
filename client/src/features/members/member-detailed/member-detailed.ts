6ae4372e18a9af7983a17c2cde82b5ac38807f55
Heugene <yttghgotthar@gmail.com>
2026-08-15 20:17:02 +0300

    Added the profile content

--- client/src/features/members/member-detailed/member-detailed.ts
+++ client/src/features/members/member-detailed/member-detailed.ts
@@ -1,7 +1,5 @@
 import { Component, inject, OnInit, Signal, signal } from '@angular/core';
-import { MemberService } from '../../../core/services/member-service';
 import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { AsyncPipe } from '@angular/common';
 import { filter, Observable, subscribeOn } from 'rxjs';
 import { Member } from '../../../types/member';
 
@@ -12,7 +10,6 @@ import { Member } from '../../../types/member';
   styleUrl: './member-detailed.css',
 })
 export class MemberDetailed implements OnInit {
-  memberService = inject(MemberService);
   private route = inject(ActivatedRoute);
   private router = inject(Router);
   protected member = signal<Member | undefined>(undefined);
