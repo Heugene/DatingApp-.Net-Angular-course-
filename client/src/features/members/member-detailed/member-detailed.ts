0c18ebc5dd97000cfdc1b1f57d9ed3d8fde4c0bc
Heugene <yttghgotthar@gmail.com>
2026-08-15 19:40:24 +0300

    Created a route resolver

--- client/src/features/members/member-detailed/member-detailed.ts
+++ client/src/features/members/member-detailed/member-detailed.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, Signal, signal } from '@angular/core';
 import { MemberService } from '../../../core/services/member-service';
 import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
@@ -7,7 +7,7 @@ import { Member } from '../../../types/member';
 
 @Component({
   selector: 'app-member-detailed',
-  imports: [AsyncPipe, RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './member-detailed.html',
   styleUrl: './member-detailed.css',
 })
@@ -15,11 +15,13 @@ export class MemberDetailed implements OnInit {
   memberService = inject(MemberService);
   private route = inject(ActivatedRoute);
   private router = inject(Router);
-  protected member$?: Observable<Member>;
+  protected member = signal<Member | undefined>(undefined);
   protected title = signal<string | undefined>('Profile');
 
   ngOnInit(): void {
-    this.member$ = this.loadMember();
+    this.route.data.subscribe({
+      next: data => this.member.set(data['member'])
+    })
     this.title.set(this.route.firstChild?.snapshot?.title);
 
     this.router.events.pipe(
@@ -29,11 +31,5 @@ export class MemberDetailed implements OnInit {
         this.title.set(this.route.firstChild?.snapshot?.title);
       }
     })
-  }
-
-  loadMember() {
-    const id = this.route.snapshot.paramMap.get('id');
-    if (!id) return;
-    return this.memberService.getMember(id);
   }
 }
