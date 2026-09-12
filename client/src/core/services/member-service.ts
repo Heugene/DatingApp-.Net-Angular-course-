daf6afa651c896f7e05d2549cd64a4e34c9da53e
Heugene <yttghgotthar@gmail.com>
2026-09-13 01:53:15 +0300

    Client image upload part 3

--- client/src/core/services/member-service.ts
+++ client/src/core/services/member-service.ts
@@ -10,7 +10,7 @@ import { tap } from 'rxjs';
 export class MemberService {
     private http = inject(HttpClient);
     private baseUrl = environment.apiUrl;
-    editMode = signal(true);
+    editMode = signal(false);
     member = signal<Member | null>(null);
 
     getMembers() {
@@ -30,7 +30,13 @@ export class MemberService {
         return this.http.get<Photo[]>(this.baseUrl + 'members/' + id + '/photos');
     }
 
-    updateMember(member: EditableMember){
+    updateMember(member: EditableMember) {
         return this.http.put(this.baseUrl + 'members', member);
+    }
+
+    uploadPhoto(file: File) {
+        const formData = new FormData();
+        formData.append('file', file);
+        return this.http.post<Photo>(this.baseUrl + "members/add-photo", formData);
     }
 }
