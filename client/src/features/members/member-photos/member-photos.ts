866d8f75daa886be2bbd1b96f52239c98f9dff67
Heugene <yttghgotthar@gmail.com>
2026-09-16 06:38:00 +0300

    Photo deletion - client

--- client/src/features/members/member-photos/member-photos.ts
+++ client/src/features/members/member-photos/member-photos.ts
@@ -7,17 +7,18 @@ import { AccountService } from '../../../core/services/account-service';
 import { User } from '../../../types/user';
 import { Member } from '../../../types/member';
 import { StarButton } from '../../../shared/star-button/star-button';
+import { DeleteButton } from '../../../shared/delete-button/delete-button';
 
 @Component({
   selector: 'app-member-photos',
-  imports: [ImageUpload, StarButton],
+  imports: [ImageUpload, StarButton, DeleteButton],
   templateUrl: './member-photos.html',
   styleUrl: './member-photos.css',
 })
 export class MemberPhotos implements OnInit {
   protected memberService = inject(MemberService);
   private route = inject(ActivatedRoute);
-  private accountService = inject(AccountService);
+  protected accountService = inject(AccountService);
 
   protected photos = signal<Photo[]>([]);
   protected loading = signal(false);
@@ -62,5 +63,13 @@ export class MemberPhotos implements OnInit {
         }) as Member)
       }
     });
+  }
+
+  deletePhoto(photoId: number) {
+    this.memberService.deletePhoto(photoId).subscribe({
+      next: () => {
+        this.photos.update(photos => photos.filter(x => x.id != photoId));
+      }
+    })
   }
 }
